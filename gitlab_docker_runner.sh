28785d5706b3b66959b1570bdb49c737e8b28725
ZarebskiDavid <zarebskidavid@gmail.com>
2019-07-12 10:45:14 +0200

    fix:gitlab-runner missing in sudoers

--- gitlab_docker_runner.sh
+++ gitlab_docker_runner.sh
@@ -20,5 +20,6 @@ ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 curl -L --output /usr/local/bin/gitlab-runner https://gitlab-runner-downloads.s3.amazonaws.com/latest/binaries/gitlab-runner-linux-amd64
 chmod +x /usr/local/bin/gitlab-runner
 useradd --comment 'GitLab Runner' --create-home gitlab-runner --shell /bin/bash
+usermod -aG docker gitlab-runner
 gitlab-runner install --user=gitlab-runner --working-directory=/home/gitlab-runner
 gitlab-runner start
